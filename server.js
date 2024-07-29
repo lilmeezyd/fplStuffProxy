b9943310bee236ae4a2686dc78b52455fb7deff3
Denis Moini <denismoini09@gmail.com>
2024-07-29 23:56:00 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,114 @@
+const axios = require('axios')
+const express = require('express')
+const port = process.env.PORT|| 5000
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors({
+    //origin: "http://localhost:3000"
+    origin: '*'
+}))
+
+app.get('/fixtures', (req, res) => {
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: `https://fantasy.premierleague.com/api/fixtures`,
+    headers: {}
+  };
+
+  axios.request(config)
+  .then((response) => {
+    res.status(200).json(response.data)
+  })
+  .catch((error) => console.log(error))
+})
+
+app.get('/bootstrap-static', (req, res) => {
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: `https://fantasy.premierleague.com/api/bootstrap-static`,
+    headers: {}
+  };
+
+  axios.request(config)
+  .then((response) => {
+    res.status(200).json(response.data)
+  })
+  .catch((error) => console.log(error))
+})
+
+app.get('/:managerId', (req, res) => {
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: `https://fantasy.premierleague.com/api/entry/${req.params.managerId}/`,
+        headers: { }
+      };
+
+      axios.request(config)
+      .then((response) => {
+        res.status(200).json(response.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+})
+
+app.get('/history/:managerId', (req, res) => {
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: `https://fantasy.premierleague.com/api/entry/${req.params.managerId}/history`,
+    headers: { }
+  };
+
+  axios.request(config)
+  .then((response) => {
+    res.status(200).json(response.data)
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+})
+
+app.get('/:managerId/event/:eventId/picks', (req, res) => {
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: `https://fantasy.premierleague.com/api/entry/${req.params.managerId}/event/${req.params.eventId}/picks/`,
+    headers: { }
+  };
+
+  axios.request(config)
+  .then((response) => {
+    res.status(200).json(response.data)
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+})
+
+app.get('/transfers/:managerId', (req, res) => {
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: `https://fantasy.premierleague.com/api/entry/${req.params.managerId}/transfers/`,
+    headers: { }
+  };
+
+  axios.request(config)
+  .then((response) => {
+    res.status(200).json(response.data)
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+})
+
+
+
+app.listen(port, console.log(`Server running at port: ${port}`))
